c12de78d1eb0c02a9ea30fc4dd97c2628443b340
Alison <alisonfaria2013@gmail.com>
2024-08-10 14:17:31 -0300

    Estrutura inicial do site

---
+++ seats.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Confirmar</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles-seats.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!-- NAVBAR -->
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">CineLight</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Combos</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Próximos lançamentos</a>
+
+                  </ul>
+                </div>
+            </div>
+        </nav> <!--NAVBAR -->
+
+                    </ul>
+                </div>
+            </div>
+        </nav> <!--NAVBAR -->
+
+    
+    <div class="container text-center">
+      <h2>Escolha seu assento:</h2>
+        <div class="row">
+          <div class="col box">
+            <form action="assentos.html" method="POST">
+                
+
+                <br>
+                <br>
+                <br>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="seats button-empty" ></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <br>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="seats button-empty" ></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <br>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="seats button-empty" ></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <br>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="seats button-empty" ></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+                <button type="button" class="btn btn-primary seats" data-bs-toggle="button"></button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <button type="submit" id="submit" name="submit" class="button-confirm">Confirmar</button>
+          </div>
+
+        </div>
+      </div>
+
+       <!-- Rodapé -->
+       <footer class="bg-dark text-center text-lg-start text-light justify-content-end">
+        <div class="container p-4">
+            <p>© 2024 CineLight. Todos os direitos reservados.</p>
+        </div>
+        </footer>
+        <!-- Rodapé -->
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
+
